d03662c1a5643f934f81b37b94815b04ed66182d
Fabian Reinartz <fab.reinartz@gmail.com>
2021-01-18 12:01:20 +0100

    Add reloader sidecar binary
    b/177810785
    
    Change-Id: Iee471f5e5c1371eb39753b26e184ac349f1f3626

---
+++ cmd/config-reloader/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/oklog/run"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/thanos-io/thanos/pkg/reloader"
+)
+
+func main() {
+	var (
+		configFile       = flag.String("config-file", "", "config file to watch for changes")
+		configFileOutput = flag.String("config-file-output", "", "config file to write with interpolated environment variables")
+		watchedDir       = flag.String("watched-dir", "", "directories to watch for file changes (for rule files)")
+		reloadURLStr     = flag.String("reload-url", "http://127.0.0.1:9090/-/reload", "Prometheus reload endpoint")
+		listenAddress    = flag.String("listen-address", ":9091", "address on which to expose metrics")
+	)
+	flag.Parse()
+
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+
+	metrics := prometheus.NewRegistry()
+	metrics.MustRegister(
+		prometheus.NewGoCollector(),
+		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
+	)
+
+	reloadURL, err := url.Parse(*reloadURLStr)
+	if err != nil {
+		level.Error(logger).Log("msg", "parsing reloader URL failed", "err", err)
+		os.Exit(1)
+	}
+
+	rel := reloader.New(
+		logger,
+		metrics,
+		&reloader.Options{
+			ReloadURL:     reloadURL,
+			CfgFile:       *configFile,
+			CfgOutputFile: *configFileOutput,
+			WatchedDirs:   []string{*watchedDir},
+			WatchInterval: 3 * time.Minute,
+			RetryInterval: 5 * time.Second,
+			DelayInterval: 3 * time.Second,
+		},
+	)
+
+	var g run.Group
+	{
+		ctx, cancel := context.WithCancel(context.Background())
+		g.Add(func() error {
+			return rel.Watch(ctx)
+		}, func(error) {
+			cancel()
+		})
+	}
+	{
+		term := make(chan os.Signal, 1)
+		cancel := make(chan struct{})
+		signal.Notify(term, os.Interrupt, syscall.SIGTERM)
+
+		g.Add(
+			func() error {
+				select {
+				case <-term:
+					level.Info(logger).Log("msg", "received SIGTERM, exiting gracefully...")
+				case <-cancel:
+				}
+				return nil
+			},
+			func(err error) {
+				close(cancel)
+			},
+		)
+	}
+	{
+		server := &http.Server{Addr: *listenAddress}
+		http.Handle("/metrics", promhttp.HandlerFor(metrics, promhttp.HandlerOpts{Registry: metrics}))
+
+		g.Add(func() error {
+			level.Info(logger).Log("msg", "Starting web server for metrics", "listen", *listenAddress)
+			return server.ListenAndServe()
+		}, func(err error) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			server.Shutdown(ctx)
+			cancel()
+		})
+	}
+
+	if err := g.Run(); err != nil {
+		level.Error(logger).Log("msg", "running reloader failed", "err", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -13,25 +13,30 @@ require (
 	github.com/prometheus/client_golang v1.7.1
 	github.com/prometheus/common v0.14.0
 	github.com/prometheus/prometheus v1.8.2-0.20201101200738-7558e9d3c374
+	github.com/thanos-io/thanos v0.17.2
 	google.golang.org/api v0.33.0
 	google.golang.org/genproto v0.0.0-20201019141844-1ed22bb0c154
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/yaml.v2 v2.3.0
-	k8s.io/api v0.19.2
-	k8s.io/apimachinery v0.19.2
-	k8s.io/client-go v0.19.2
-	k8s.io/code-generator v0.20.2
+	k8s.io/api v0.20.1
+	k8s.io/apimachinery v0.20.1
+	k8s.io/client-go v12.0.0+incompatible
+	k8s.io/code-generator v0.20.1
 	k8s.io/klog v1.0.0
-	k8s.io/klog/v2 v2.3.0
+	k8s.io/klog/v2 v2.4.0
 )
 
 replace (
-	k8s.io/api => k8s.io/api v0.18.8
-	k8s.io/apimachinery => k8s.io/apimachinery v0.18.8
-	k8s.io/client-go => k8s.io/client-go v0.18.8
-	k8s.io/code-generator => k8s.io/code-generator v0.18.8
+	// Requiring the version above is not enough, probably because transitive dependencies
+	// may be allowed to use an older version without the below lines, which causes
+	// lookup errors on vendoring.
+	k8s.io/api => k8s.io/api v0.20.1
+	k8s.io/apimachinery => k8s.io/apimachinery v0.20.1
+	k8s.io/client-go => k8s.io/client-go v0.20.1
+	k8s.io/code-generator => k8s.io/code-generator v0.20.1
+
 	// Substitute klog packages so their usage plays nicely with gokit's logger.
 	k8s.io/klog => github.com/simonpasquier/klog-gokit v0.3.0
 	k8s.io/klog/v2 => github.com/simonpasquier/klog-gokit/v2 v2.0.1
