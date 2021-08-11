0fcb45905157d3c911d908455201862bb071d2af
Daniel Clark <danielclark@google.com>
2021-08-11 19:34:34 +0000

    Use controller-gen to generate openapi spec
    - First manually fix CRD API to conform to v1 (pretty different from
    v1beta1).
    - Add script and makefile target that rely on controller-gen, which
    seemed to be the recommended way to programmatically generate CRD yamls
    from go structs.
    - Had to do some hacky stuff here. Since controller-gen doesn't appear to work
    properly with grouped k8s resources in a single directory, I split the
    consolidated crds yaml by '---' into separate files in separate temporary
    directories, run controller-gen to merge the output on each CRD directory
    separately, then concatonate them back together and overwrite the consolidated
    yaml. Ultimately, we should maybe contribute a fix upstream and/or try
    using the kubebuilder format...

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -23,7 +23,6 @@ SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 # We want to version the code generation with our Go modules to avoid incompatibilities.
 # Thus, while we don't want to use a vendor/ directory in general, we create it once to make
 # this script work. Afterwards we delete it again.
-trap "rm -rf $SCRIPT_ROOT/vendor/" EXIT
 go mod vendor
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
---
+++ hack/update-crdgen.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Generating CRD yamls"
+
+which controller-gen || go install sigs.k8s.io/controller-tools/cmd/controller-gen@latest
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CRD_DIR=${SCRIPT_ROOT}/cmd/operator/deploy/operator
+CRD_TMP=$(mktemp -d)
+
+# Split current crds.yaml into individual CRD files.
+csplit --quiet -f ${CRD_TMP}/crd- -b "%02d.yaml" ${CRD_DIR}/crds.yaml "/---/+1" "{*}"
+
+# Re-generate each CRD patch separately (limitation of controller-gen).
+CRD_TMPS=$(find $CRD_TMP -iname '*.yaml' | sort)
+for i in $CRD_TMPS; do
+  b=$(basename ${i})
+  dir=${i%.yaml}
+  mkdir -p ${dir}
+  mv $i ${dir}/$b
+  controller-gen schemapatch:manifests=${dir} output:dir=${dir} paths=./pkg/operator/apis/...
+done
+
+# Merge and overwrite crds.yaml.
+CRD_TMPS=$(find $CRD_TMP -iname '*.yaml' | sort)
+sed -s '$a---' $CRD_TMPS > ${CRD_DIR}/crds-tmp.yaml
+mv ${CRD_DIR}/crds-tmp.yaml ${CRD_DIR}/crds.yaml
