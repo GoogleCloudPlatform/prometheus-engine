48a160304ba8c3e395794698c8949b1ba7a47852
Fabian Reinartz <freinartz@google.com>
2021-09-06 17:44:37 +0200

    Revert false quoting
    Passing flags via a manifest removes the need for quoting, i.e. added
    quotes are passed through verbatim and cause bad project and location
    identifiers.

--- pkg/operator/collection.go
+++ pkg/operator/collection.go
@@ -195,10 +195,10 @@ func (r *collectionReconciler) makeCollectorDaemonSet() *appsv1.DaemonSet {
 
 	// Check for explicitly-set pass-through args.
 	if r.opts.ProjectID != "" {
-		collectorArgs = append(collectorArgs, fmt.Sprintf("--export.label.project-id=%q", r.opts.ProjectID))
+		collectorArgs = append(collectorArgs, fmt.Sprintf("--export.label.project-id=%s", r.opts.ProjectID))
 	}
 	if r.opts.Location != "" {
-		collectorArgs = append(collectorArgs, fmt.Sprintf("--export.label.location=%q", r.opts.ProjectID))
+		collectorArgs = append(collectorArgs, fmt.Sprintf("--export.label.location=%s", r.opts.ProjectID))
 	}
 	if r.opts.DisableExport {
 		collectorArgs = append(collectorArgs, "--export.disable")
