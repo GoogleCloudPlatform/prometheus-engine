c8137fe09f526906d4f626efe8fac2740e3f7b13
Andy <andysim3d@gmail.com>
2022-03-28 11:09:11 -0400

    bump the version to 0.3.3

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -72,9 +72,9 @@ const (
 
 	// The official images to be used with this version of the operator. For debugging
 	// and emergency use cases they may be overwritten through options.
-	ImageCollector      = "gke.gcr.io/prometheus-engine/prometheus:v2.28.1-gmp.5-gke.0"
-	ImageConfigReloader = "gke.gcr.io/prometheus-engine/config-reloader:v0.3.1-gke.0"
-	ImageRuleEvaluator  = "gke.gcr.io/prometheus-engine/rule-evaluator:v0.3.1-gke.0"
+	ImageCollector      = "gke.gcr.io/prometheus-engine/prometheus:v2.28.1-gmp.7-gke.0"
+	ImageConfigReloader = "gke.gcr.io/prometheus-engine/config-reloader:v0.3.3-gke.0"
+	ImageRuleEvaluator  = "gke.gcr.io/prometheus-engine/rule-evaluator:v0.3.3-gke.0"
 
 	// The k8s Application, will be exposed as component name.
 	KubernetesAppName    = "app"
