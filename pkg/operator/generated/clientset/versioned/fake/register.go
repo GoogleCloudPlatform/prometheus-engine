a5ba1595c48dac42f97a354c3fedbc4d97e6bd2a
Daniel Clark <danielclark@google.com>
2022-04-07 17:23:20 +0000

    auto-generated files via `make ps`

--- pkg/operator/generated/clientset/versioned/fake/register.go
+++ pkg/operator/generated/clientset/versioned/fake/register.go
@@ -17,7 +17,7 @@
 package fake
 
 import (
-	monitoringv1alpha1 "github.com/GoogleCloudPlatform/prometheus-engine/pkg/operator/apis/monitoring/v1alpha1"
+	monitoringv1 "github.com/GoogleCloudPlatform/prometheus-engine/pkg/operator/apis/monitoring/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
@@ -29,7 +29,7 @@ var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
 
 var localSchemeBuilder = runtime.SchemeBuilder{
-	monitoringv1alpha1.AddToScheme,
+	monitoringv1.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition
