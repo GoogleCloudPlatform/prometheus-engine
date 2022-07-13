56db8820cf758f4a637e79e8aa72834c311b596a
Joshua Pollock <joshuapollock@google.com>
2022-07-13 15:43:11 +0000

    add cmd/fake-metric-server

---
+++ cmd/fake-metric-server/main.go
@@ -0,0 +1,45 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	"github.com/GoogleCloudPlatform/prometheus-engine/pkg/e2e"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+	"google.golang.org/grpc"
+)
+
+var (
+	maxTimeSeriesPerRequest = flag.Int("max-time-series-per-request", 200,
+		"The maximum amount of time series we can send per CreateTimeSeries. Default is 200.")
+	port = flag.String("port", ":8080", "The port to listen for requests on.")
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", *port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Println("Listening on ", *port)
+	serv := grpc.NewServer()
+	monitoringpb.RegisterMetricServiceServer(serv, e2e.NewFakeMetricServer(*maxTimeSeriesPerRequest))
+	if err := serv.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
