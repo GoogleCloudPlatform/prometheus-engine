013afbd59e9e7a570d18af45f4fc3327f685a99b
Daniel Hrabovcak <thespiritxiii@gmail.com>
2023-09-26 16:24:04 -0400

    Parallelize webhook tests and replace clientsets

--- e2e/webhook_test.go
+++ e2e/webhook_test.go
@@ -22,10 +22,12 @@ import (
 	arv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // webhook configurations.
 func TestWebhookCABundleInjection(t *testing.T) {
+	t.Parallel()
 	tctx := newOperatorContext(t)
 
 	var (
@@ -35,94 +37,97 @@ func TestWebhookCABundleInjection(t *testing.T) {
 		url          = "https://0.1.2.3/"
 	)
 
-	// Create webhook configs. The operator must populate their caBundles.
-	vwc := &arv1.ValidatingWebhookConfiguration{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   whConfigName,
-			Labels: tctx.getSubTestLabels(),
-		},
-		Webhooks: []arv1.ValidatingWebhook{
-			{
-				Name:                    "wh1.monitoring.googleapis.com",
-				ClientConfig:            arv1.WebhookClientConfig{URL: &url},
-				FailurePolicy:           &policy,
-				SideEffects:             &sideEffects,
-				AdmissionReviewVersions: []string{"v1"},
-			}, {
-				Name:                    "wh2.monitoring.googleapis.com",
-				ClientConfig:            arv1.WebhookClientConfig{URL: &url},
-				FailurePolicy:           &policy,
-				SideEffects:             &sideEffects,
-				AdmissionReviewVersions: []string{"v1"},
+	t.Run("validating webhook", tctx.subtest(func(ctx context.Context, t *OperatorContext) {
+		t.Parallel()
+		// Create webhook configs. The operator must populate their caBundles.
+		vwc := &arv1.ValidatingWebhookConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: whConfigName,
 			},
-		},
-	}
-	_, err := tctx.kubeClient.AdmissionregistrationV1().ValidatingWebhookConfigurations().Create(context.Background(), vwc, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	mwc := &arv1.MutatingWebhookConfiguration{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   whConfigName,
-			Labels: tctx.getSubTestLabels(),
-		},
-		Webhooks: []arv1.MutatingWebhook{
-			{
-				Name:                    "wh1.monitoring.googleapis.com",
-				ClientConfig:            arv1.WebhookClientConfig{URL: &url},
-				FailurePolicy:           &policy,
-				SideEffects:             &sideEffects,
-				AdmissionReviewVersions: []string{"v1"},
-			}, {
-				Name:                    "wh2.monitoring.googleapis.com",
-				ClientConfig:            arv1.WebhookClientConfig{URL: &url},
-				FailurePolicy:           &policy,
-				SideEffects:             &sideEffects,
-				AdmissionReviewVersions: []string{"v1"},
+			Webhooks: []arv1.ValidatingWebhook{
+				{
+					Name:                    "wh1.monitoring.googleapis.com",
+					ClientConfig:            arv1.WebhookClientConfig{URL: &url},
+					FailurePolicy:           &policy,
+					SideEffects:             &sideEffects,
+					AdmissionReviewVersions: []string{"v1"},
+				}, {
+					Name:                    "wh2.monitoring.googleapis.com",
+					ClientConfig:            arv1.WebhookClientConfig{URL: &url},
+					FailurePolicy:           &policy,
+					SideEffects:             &sideEffects,
+					AdmissionReviewVersions: []string{"v1"},
+				},
 			},
-		},
-	}
-	_, err = tctx.kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Create(context.Background(), mwc, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
+		}
+		if err := tctx.Client().Create(ctx, vwc); err != nil {
+			t.Fatal(err)
+		}
 
-	// Wait for caBundle injection.
-	err = wait.Poll(3*time.Second, 2*time.Minute, func() (bool, error) {
-		vwc, err := tctx.kubeClient.AdmissionregistrationV1().ValidatingWebhookConfigurations().Get(context.Background(), whConfigName, metav1.GetOptions{})
-		if err != nil {
-			return false, fmt.Errorf("get validatingwebhook configuration: %w", err)
-		}
-		if len(vwc.Webhooks) != 2 {
-			return false, fmt.Errorf("expected 2 webhooks but got %d", len(vwc.Webhooks))
-		}
-		for _, wh := range vwc.Webhooks {
-			if len(wh.ClientConfig.CABundle) == 0 {
-				return false, nil
+		// Wait for caBundle injection.
+		err := wait.Poll(3*time.Second, 2*time.Minute, func() (bool, error) {
+			if err := tctx.Client().Get(ctx, client.ObjectKeyFromObject(vwc), vwc); err != nil {
+				return false, fmt.Errorf("get validatingwebhook configuration: %w", err)
 			}
+			if len(vwc.Webhooks) != 2 {
+				return false, fmt.Errorf("expected 2 webhooks but got %d", len(vwc.Webhooks))
+			}
+			for _, wh := range vwc.Webhooks {
+				if len(wh.ClientConfig.CABundle) == 0 {
+					return false, nil
+				}
+			}
+			return true, nil
+		})
+		if err != nil {
+			t.Fatalf("waiting for ValidatingWebhook CA bundle failed: %s", err)
 		}
-		return true, nil
-	})
-	if err != nil {
-		t.Fatalf("waiting for ValidatingWebhook CA bundle failed: %s", err)
-	}
+	}))
 
-	err = wait.Poll(3*time.Second, 2*time.Minute, func() (bool, error) {
-		mwc, err := tctx.kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.Background(), whConfigName, metav1.GetOptions{})
-		if err != nil {
-			return false, fmt.Errorf("get mutatingwebhook configuration: %w", err)
+	t.Run("mutating webhook", tctx.subtest(func(ctx context.Context, t *OperatorContext) {
+		t.Parallel()
+		// Create webhook configs. The operator must populate their caBundles.
+		mwc := &arv1.MutatingWebhookConfiguration{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: whConfigName,
+			},
+			Webhooks: []arv1.MutatingWebhook{
+				{
+					Name:                    "wh1.monitoring.googleapis.com",
+					ClientConfig:            arv1.WebhookClientConfig{URL: &url},
+					FailurePolicy:           &policy,
+					SideEffects:             &sideEffects,
+					AdmissionReviewVersions: []string{"v1"},
+				}, {
+					Name:                    "wh2.monitoring.googleapis.com",
+					ClientConfig:            arv1.WebhookClientConfig{URL: &url},
+					FailurePolicy:           &policy,
+					SideEffects:             &sideEffects,
+					AdmissionReviewVersions: []string{"v1"},
+				},
+			},
 		}
-		if len(mwc.Webhooks) != 2 {
-			return false, fmt.Errorf("expected 2 webhooks but got %d", len(vwc.Webhooks))
+		if err := tctx.Client().Create(ctx, mwc); err != nil {
+			t.Fatal(err)
 		}
-		for _, wh := range mwc.Webhooks {
-			if len(wh.ClientConfig.CABundle) == 0 {
-				return false, nil
+
+		// Wait for caBundle injection.
+		err := wait.Poll(3*time.Second, 2*time.Minute, func() (bool, error) {
+			if err := tctx.Client().Get(ctx, client.ObjectKeyFromObject(mwc), mwc); err != nil {
+				return false, fmt.Errorf("get mutatingwebhook configuration: %w", err)
 			}
+			if len(mwc.Webhooks) != 2 {
+				return false, fmt.Errorf("expected 2 webhooks but got %d", len(mwc.Webhooks))
+			}
+			for _, wh := range mwc.Webhooks {
+				if len(wh.ClientConfig.CABundle) == 0 {
+					return false, nil
+				}
+			}
+			return true, nil
+		})
+		if err != nil {
+			t.Fatalf("waiting for MutatingWebhook CA bundle failed: %s", err)
 		}
-		return true, nil
-	})
-	if err != nil {
-		t.Fatalf("waiting for MutatingWebhook CA bundle failed: %s", err)
-	}
+	}))
 }
