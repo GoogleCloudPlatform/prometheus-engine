21d5fae73a262a1bc5f582e4a010d04775273819
Lee Yanco <lyanco@google.com>
2023-12-05 11:26:58 -0500

    Create delete_metric_descriptors.go (#679)
    * Create delete_metric_descriptors.go
    
    Add the metric deletion script to the repo so we can refer to it in troubleshooting documentation
    
    * Tests + fixes to the script.
    
    Signed-off-by: bwplotka <bwplotka@google.com>
    
    * Addressed comments.
    
    Signed-off-by: bwplotka <bwplotka@google.com>
    
    * Addressed comments.
    
    Signed-off-by: bwplotka <bwplotka@google.com>
    
    * Addressed comment.
    
    Signed-off-by: bwplotka <bwplotka@google.com>
    
    ---------
    
    Signed-off-by: bwplotka <bwplotka@google.com>
    Co-authored-by: bwplotka <bwplotka@google.com>

---
+++ examples/scripts/delete_metric_descriptors/delete_metric_descriptors.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+	"unicode"
+
+	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
+	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+/*
+This script deletes metric descriptors from the given projects (-projects flag),
+matching the given metric type (descriptor name) regex expression (-metric_type_regex flag).
+
+Metrics to delete will be first printed and then awaiting interactive confirmation,
+before the actual removal. Dry run option also exists.
+
+WARNING: All underlying time series behind each descriptor (potentially years
+of data) will be irreversibly removed once confirmed.
+
+Example run:
+
+1. Setup Application Default Credentials (ADC) (https://cloud.google.com/docs/authentication/provide-credentials-adc)
+if you haven't yet:
+	1a. Make sure the account behind the ADC for chosen projects has Monitoring Editor or Monitoring Admin permissions: https://cloud.google.com/monitoring/access-control#monitoring-perms
+  1b. Acquire Application Default Credentials in your environment using gcloud:
+
+gcloud auth application-default login
+
+2. Run Go script (from the same directory as the script):
+
+go run delete_metric_descriptors.go -projects projects/<your-project> -metric_type_regex "<your matching expression>"
+
+See go run delete_metric_descriptors.go -help for all options.
+*/
+
+var (
+	cloudMonitoringEndpoint = flag.String("address", "monitoring.googleapis.com:443", "address of monitoring API")
+
+	projectNames    = flag.String("projects", "", "required: comma-separated project IDs of the projects on which to execute the requests. Name format is as defined in https://cloud.google.com/monitoring/api/ref_v3/rpc/google.monitoring.v3#listmetricdescriptorsrequesttarget, e.g. projects/test-project,projects/test-project2")
+	metricTypeRegex = flag.String("metric_type_regex", "", "required: RE2 regex expression matching metric.type (anchored), so metric descriptor names to delete. Guarded with the interactive 'y' confirmation. See --dry_run to only print those")
+	dryRun          = flag.Bool("dry_run", false, "whether to dry run or not")
+
+	serviceAccountEnvVar = flag.String("sa-envvar", "", "optional environment variable containing Google Service Account JSON, without it application-default flow will be used.")
+)
+
+func deleteDescriptors(endpoint string, projects []string, re *regexp.Regexp, saEnvVar string, dryRun bool) error {
+	ctx := context.Background()
+
+	// Recommended way is to use auth from your environment. Use `gcloud auth application-default login` to set it up.
+	client, err := monitoring.NewMetricClient(ctx, func() []option.ClientOption {
+		// Optional, service account JSON in environment variable.
+		if saEnvVar != "" {
+			return []option.ClientOption{
+				option.WithEndpoint(endpoint),
+				option.WithCredentialsJSON([]byte(os.Getenv(saEnvVar))),
+			}
+		}
+		return []option.ClientOption{option.WithEndpoint(endpoint)}
+	}()...)
+	if err != nil {
+		return fmt.Errorf("failed to build client for %s: %w", endpoint, err)
+	}
+	defer client.Close()
+
+	// Find descriptors to delete.
+	descsToDelete := map[string][]string{}
+	toDelete := 0
+	checked := 0
+	for _, p := range projects {
+		it := client.ListMetricDescriptors(ctx, &monitoringpb.ListMetricDescriptorsRequest{Name: p})
+		for {
+			resp, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return fmt.Errorf("ListMetricDescriptors iteration: %w", err)
+			}
+			checked++
+			if !re.MatchString(resp.Type) {
+				continue
+			}
+			descsToDelete[p] = append(descsToDelete[p], resp.Type)
+			toDelete++
+		}
+	}
+
+	// Print and perform interactive safety check.
+	{
+		for p, descs := range descsToDelete {
+			fmt.Println()
+			fmt.Printf("For project %v:\n", p)
+			fmt.Println(descs)
+		}
+		fmt.Printf("After checking %v descriptors, found %v to delete across %v project(s)\n", checked, toDelete, len(projects))
+		fmt.Println()
+	}
+	if toDelete == 0 {
+		fmt.Println("nothing to do, job done!")
+		return nil
+	}
+	if dryRun {
+		fmt.Println("-dry_run selected, job done!")
+		return nil
+	}
+	if !confirmDelete() {
+		fmt.Println("Deletion not confirmed, exiting")
+		return nil
+	}
+
+	// Delete.
+	deleted := 0
+	for p, descs := range descsToDelete {
+		for _, d := range descs {
+			if err := client.DeleteMetricDescriptor(ctx,
+				&monitoringpb.DeleteMetricDescriptorRequest{
+					Name: fmt.Sprintf("%s/metricDescriptors/%s", p, d),
+				}); err != nil {
+				return fmt.Errorf("DeleteMetricDescriptor delete: %w", err)
+			}
+			deleted++
+			fmt.Printf("%s deleted\n", d)
+			if deleted%1000 == 0 {
+				fmt.Println("Sleeping 1 second to avoid quota issues...")
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}
+	fmt.Printf("Deleted %v descriptors, job done!\n", deleted)
+	return nil
+}
+
+func confirmDelete() bool {
+	fmt.Printf("Are you sure you want to delete the above metric descriptors?\n" +
+		"WARNING: All underlying time series (potentially years of data) will be irreversibly removed! (y/N): ")
+	r, _, err := bufio.NewReader(os.Stdin).ReadRune()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	switch unicode.ToLower(r) {
+	case 'y':
+		return true
+	default:
+		return false
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *projectNames == "" {
+		fmt.Println("-projects flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *metricTypeRegex == "" {
+		fmt.Println("-metric_type_regex flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	// Anchor it to avoid further surprises.
+	reExpr := fmt.Sprintf("^%s$", *metricTypeRegex)
+	re, err := regexp.Compile(reExpr)
+	if err != nil {
+		log.Fatalf("error while compiling RE2 %v expression: %v", *metricTypeRegex, err)
+	}
+	// Run command.
+	if err := deleteDescriptors(
+		*cloudMonitoringEndpoint,
+		strings.Split(*projectNames, ","),
+		re,
+		*serviceAccountEnvVar,
+		*dryRun,
+	); err != nil {
+		log.Fatalf("command failed: %v", err)
+	}
+}
---
+++ examples/scripts/scripts_test.go
@@ -0,0 +1,168 @@
+package scripts
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/google/go-cmp/cmp"
+	"github.com/oklog/ulid"
+	"google.golang.org/api/iterator"
+	monitoredres_pb "google.golang.org/genproto/googleapis/api/monitoredres"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	gcm "cloud.google.com/go/monitoring/apiv3/v2"
+	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
+	"github.com/efficientgo/core/runutil"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	metricpb "google.golang.org/genproto/googleapis/api/metric"
+)
+
+// GCMServiceAccountOrFail gets the Google SA JSON content from GCM_SECRET
+// environment variable or fails.
+func gcmServiceAccountOrFail(t testing.TB) []byte {
+	// TODO(bwplotka): Move it to https://cloud.google.com/build CI.
+	saJSON := []byte(os.Getenv("GCM_SECRET"))
+	if len(saJSON) == 0 {
+		t.Fatal("gcmServiceAccountOrFail: no GCM_SECRET env var provided, can't run the test")
+	}
+	return saJSON
+}
+
+func assertExpectedTypesExists(t *testing.T, client *gcm.MetricClient, reqName string, expectedTypes []string) {
+	t.Helper()
+
+	tctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := runutil.RetryWithLog(log.NewLogfmtLogger(os.Stderr), 200*time.Second, tctx.Done(), func() error {
+		it := client.ListMetricDescriptors(
+			tctx,
+			&monitoringpb.ListMetricDescriptorsRequest{
+				Name:   reqName,
+				Filter: fmt.Sprintf(`metric.type = starts_with("%s")`, expectedTypes[0]),
+			},
+		)
+		var foundTypes []string
+		for {
+			resp, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			foundTypes = append(foundTypes, resp.Type)
+		}
+		if diff := cmp.Diff(expectedTypes, foundTypes); diff != "" {
+			return fmt.Errorf("expected different types: %v", diff)
+		}
+		return nil
+	}); err != nil {
+		t.Fatalf("check if available: %v", err)
+	}
+}
+
+func TestDeleteMetricDescriptorsDryRun(t *testing.T) {
+	gcmSA := gcmServiceAccountOrFail(t)
+
+	ctx := context.Background()
+	creds, err := google.CredentialsFromJSON(ctx, gcmSA, gcm.DefaultAuthScopes()...)
+	if err != nil {
+		t.Fatalf("create credentials from JSON: %s", err)
+	}
+
+	// Mimic export user-agent.
+	ua := "prometheus-engine-script-test"
+	client, err := gcm.NewMetricClient(
+		ctx,
+		option.WithCredentials(creds),
+		option.WithUserAgent(ua))
+	if err != nil {
+		t.Fatalf("create client: %s", err)
+	}
+	t.Cleanup(func() { _ = client.Close() })
+
+	testID := fmt.Sprintf("%v: %v", t.Name(), ulid.MustNew(ulid.Now(), rand.New(rand.NewSource(time.Now().UnixNano()))).String())
+
+	var expectedTypes = []string{
+		"prometheus.googleapis.com/pe_test_script", "prometheus.googleapis.com/pe_test_script2",
+	}
+
+	reqName := "projects/" + creds.ProjectID
+	resource := &monitoredres_pb.MonitoredResource{
+		Type: "prometheus_target",
+		Labels: map[string]string{
+			"project_id": creds.ProjectID,
+			"cluster":    "pe-github-action",
+			"location":   "europe-west3-a",
+			"job":        "",
+			"instance":   "",
+			"namespace":  "",
+		},
+	}
+	now := time.Now()
+	// Writing test data.
+	if err := client.CreateTimeSeries(ctx, &monitoringpb.CreateTimeSeriesRequest{
+		Name: reqName,
+		TimeSeries: []*monitoringpb.TimeSeries{
+			{
+				Resource:   resource,
+				Metric:     &metricpb.Metric{Type: expectedTypes[0], Labels: map[string]string{"test": testID}},
+				MetricKind: metricpb.MetricDescriptor_GAUGE,
+				ValueType:  metricpb.MetricDescriptor_DOUBLE,
+
+				Points: []*monitoringpb.Point{{
+					Interval: &monitoringpb.TimeInterval{StartTime: timestamppb.New(now), EndTime: timestamppb.New(now)},
+					Value:    &monitoringpb.TypedValue{Value: &monitoringpb.TypedValue_DoubleValue{DoubleValue: 0.1}}}},
+			},
+			{
+				Resource:   resource,
+				Metric:     &metricpb.Metric{Type: expectedTypes[1], Labels: map[string]string{"test": testID}},
+				MetricKind: metricpb.MetricDescriptor_GAUGE,
+				ValueType:  metricpb.MetricDescriptor_DOUBLE,
+
+				Points: []*monitoringpb.Point{{
+					Interval: &monitoringpb.TimeInterval{StartTime: timestamppb.New(now), EndTime: timestamppb.New(now)},
+					Value:    &monitoringpb.TypedValue{Value: &monitoringpb.TypedValue_DoubleValue{DoubleValue: 0.1}}}},
+			},
+		},
+	}); err != nil {
+		t.Fatalf("test data write: %v", err)
+	}
+
+	assertExpectedTypesExists(t, client, reqName, expectedTypes)
+
+	b := bytes.Buffer{}
+	cmd := exec.Command(
+		"go", "run",
+		"delete_metric_descriptors/delete_metric_descriptors.go",
+		"-projects", "projects/"+creds.ProjectID,
+		"-metric_type_regex", ".*pe_test_script(|2)",
+		"-dry_run",
+		"-sa-envvar", "GCM_SECRET",
+	)
+	cmd.Stderr, cmd.Stdout = &b, &b
+	if err := cmd.Run(); err != nil {
+		fmt.Println(b.String())
+		t.Fatal(err)
+	}
+	if expPrefix, got := "\nFor project projects/gpe-test-1:\n[prometheus.googleapis.com/pe_test_script prometheus.googleapis.com/pe_test_script2]\nAfter checking", b.String(); !strings.HasPrefix(got, expPrefix) {
+		fmt.Println(cmp.Diff(expPrefix, got))
+		t.Fatalf("got %v, does not have expected prefix %v", got, expPrefix)
+	}
+	if expSuffix, got := "descriptors, found 2 to delete across 1 project(s)\n\n-dry_run selected, job done!\n", b.String(); !strings.HasSuffix(got, expSuffix) {
+		t.Fatalf("got %v, does not have expected suffix %v", got, expSuffix)
+	}
+
+	// Nothing should be deleted.
+	assertExpectedTypesExists(t, client, reqName, expectedTypes)
+
+}
