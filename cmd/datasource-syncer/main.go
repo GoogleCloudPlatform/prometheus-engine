4697b13d81baa95566e9692a6c7872a0fd7ba184
Max Amin <maxamin@google.com>
2024-01-18 16:31:36 -0500

    Add TLS to datasource-syncer (#768)
    1. Added TLS support, including four new flags `tls-cert`, `tls-key`, `tls-ca-cert` and `insecure-skip-verify`.
    2. Added a condition to return error and exist if any data source syncs fail.
    3. Added a label to datasource-syncer.yaml so users can use `kubectl get pods -l app=datasource-syncer` to view pods associated with their job.

--- cmd/datasource-syncer/main.go
+++ cmd/datasource-syncer/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"net/http"
@@ -11,6 +13,7 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	grafana "github.com/grafana/grafana-api-golang-client"
+	"github.com/hashicorp/go-cleanhttp"
 	"golang.org/x/mod/semver"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -32,6 +35,11 @@ var (
 
 	gcmEndpointOverride = flag.String("gcm-endpoint-override", "",
 		"gcm-endpoint-override is the URL where queries should be sent to from Grafana. This should be left blank in almost all circumstances.")
+
+	certFile           = flag.String("tls-cert", "", "Path to the server TLS certificate.")
+	keyFile            = flag.String("tls-key", "", "Path to the server TLS key.")
+	caFile             = flag.String("tls-ca-cert", "", "Path to the server certificate authority")
+	insecureSkipVerify = flag.Bool("insecure-skip-verify", false, "Skip TLS certificate verification")
 )
 
 func main() {
@@ -60,7 +68,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	grafanaClient, err := grafana.New(*grafanaEndpoint, grafana.Config{APIKey: *grafanaAPIToken})
+	client, err := getTLSClient(*certFile, *keyFile, *caFile, *insecureSkipVerify)
+	if err != nil {
+		level.Error(logger).Log("msg", "couldn't create client", "err", err)
+		os.Exit(1)
+	}
+
+	grafanaClient, err := grafana.New(*grafanaEndpoint, grafana.Config{
+		APIKey: *grafanaAPIToken,
+		Client: client,
+	})
 	if err != nil {
 		level.Error(logger).Log("msg", "couldn't create grafana client", "err", err)
 		os.Exit(1)
@@ -73,6 +90,7 @@ func main() {
 	}
 
 	dsSuccessfullyUpdated := []string{}
+	dsErrors := []string{}
 	datasourceUIDs := strings.Split(*datasourceUIDList, ",")
 	for _, datasourceUID := range datasourceUIDs {
 		datasourceUID = strings.TrimSpace(datasourceUID)
@@ -82,25 +100,32 @@ func main() {
 
 		dataSource, err := grafanaClient.DataSourceByUID(datasourceUID)
 		if err != nil {
+			dsErrors = append(dsErrors, datasourceUID)
 			level.Error(logger).Log("msg", fmt.Sprintf("error fetching data source config of data source uid: %s", datasourceUID), "err", err)
 			continue
 		}
 
 		dataSource, err = buildUpdateDataSourceRequest(*dataSource, token)
 		if err != nil {
+			dsErrors = append(dsErrors, datasourceUID)
 			level.Error(logger).Log("msg", fmt.Sprintf("couldn't build data source update request for data source uid: %s", datasourceUID), "err", err)
 			continue
 		}
 
 		err = grafanaClient.UpdateDataSourceByUID(dataSource)
 		if err != nil {
+			dsErrors = append(dsErrors, datasourceUID)
 			level.Error(logger).Log("msg", fmt.Sprintf("couldn't send update data source request to data source id: %s", datasourceUID), "err", err)
 			continue
 		}
 		dsSuccessfullyUpdated = append(dsSuccessfullyUpdated, datasourceUID)
 	}
 	if len(dsSuccessfullyUpdated) != 0 {
-		level.Info(logger).Log("msg", fmt.Sprintf("Updated grafana data source uids: %s", dsSuccessfullyUpdated))
+		level.Info(logger).Log("msg", fmt.Sprintf("Updated Grafana data source uids: %s", dsSuccessfullyUpdated))
+	}
+	if len(dsErrors) != 0 {
+		level.Error(logger).Log("msg", fmt.Sprintf("Failed to update Grafana data source uids: %s", dsErrors))
+		os.Exit(1)
 	}
 }
 
@@ -221,4 +246,43 @@ func buildUpdateDataSourceRequest(dataSource grafana.DataSource, token string) (
 	// Add token to SecureJSONData e.g. httpHeaderValue1: Bearer 123.
 	dataSource.SecureJSONData[authHeaderValue] = fmt.Sprintf("Bearer %s", token)
 	return &dataSource, nil
+}
+
+func getTLSClient(certFile, keyFile, caFile string, insecureSkipVerify bool) (*http.Client, error) {
+	if (certFile != "" || keyFile != "") && (certFile == "" || keyFile == "") {
+		return nil, fmt.Errorf("--tls-cert and tls-key must both be set or unset")
+	}
+
+	if certFile == "" && keyFile == "" && caFile == "" && !insecureSkipVerify {
+		return nil, nil
+	}
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: insecureSkipVerify,
+	}
+
+	if certFile != "" && keyFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return nil, fmt.Errorf("unable to load server cert and key: %w", err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
+	if caFile != "" {
+		caCert, err := os.ReadFile(caFile)
+		if err != nil {
+			return nil, fmt.Errorf("unable to read ca cert: %w", err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		tlsConfig.RootCAs = caCertPool
+	}
+
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.TLSClientConfig = tlsConfig
+
+	client := cleanhttp.DefaultClient()
+	client.Transport = transport
+	return client, nil
 }
