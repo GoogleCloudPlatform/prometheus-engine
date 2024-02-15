4c0340bd17e849e3b2832472fc768e9a41ebf863
Adam Bernot <bernot@google.com>
2024-02-15 16:29:04 +0000

    chore: preparing for v0.11.0-rc.0 release

--- pkg/export/export.go
+++ pkg/export/export.go
@@ -530,7 +530,7 @@ const (
 	ClientName = "prometheus-engine-export"
 	// mainModuleVersion is the version of the main module. Align with git tag.
 	// TODO(TheSpiritXIII): Remove with https://github.com/golang/go/issues/50603
-	mainModuleVersion = "v0.10.0-rc.1"
+	mainModuleVersion = "v0.11.0-rc.0"
 	// mainModuleName is the name of the main module. Align with go.mod.
 	mainModuleName = "github.com/GoogleCloudPlatform/prometheus-engine"
 )
