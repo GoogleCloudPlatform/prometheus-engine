be240f331a99b81b7971318f69ad1d675fb0889f
Daniel Hrabovcak <thespiritxiii@gmail.com>
2024-02-16 15:59:16 -0500

    refactor(e2e): move tag copying to Go and use exact tags

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -20,12 +20,14 @@ package e2e
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"testing"
 	"time"
 
+	dockerclient "github.com/docker/docker/client"
 	"go.uber.org/zap/zapcore"
 	metav1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
@@ -41,14 +43,21 @@ import (
 	"github.com/GoogleCloudPlatform/prometheus-engine/e2e/kube"
 	"github.com/GoogleCloudPlatform/prometheus-engine/pkg/operator"
 	"github.com/GoogleCloudPlatform/prometheus-engine/pkg/operator/generated/clientset/versioned"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 )
 
 var (
+	images = [...]string{"operator", "config-reloader", "rule-evaluator", "example-app"}
+
 	projectID, location, cluster string
 	skipGCM                      bool
 	pollDuration                 time.Duration
 
 	gcpServiceAccount string
+
+	imageRegistryPort int
+	imageTag          string
 )
 
 // TestMain injects custom flags to tests.
@@ -63,13 +72,23 @@ func TestMain(m *testing.M) {
 
 	flag.StringVar(&gcpServiceAccount, "gcp-service-account", "", "Path to GCP service account file for usage by deployed containers.")
 
+	flag.IntVar(&imageRegistryPort, "registry-port", -1, "The port of the local registry.")
+	flag.StringVar(&imageTag, "image-tag", "", "The tag to copy images from.")
+
 	flag.Parse()
 
 	os.Exit(m.Run())
 }
 
-func setupCluster(ctx context.Context, t testing.TB) (kubernetes.Interface, versioned.Interface, error) {
-	t.Log(">>> deploying static resources")
+func setupCluster(ctx context.Context, logger log) (kubernetes.Interface, versioned.Interface, error) {
+	if imageTag != "" && imageRegistryPort >= 0 {
+		if err := copyImagesToLocalRegistry(ctx, logger, imageTag, imageRegistryPort); err != nil {
+			return nil, nil, err
+		}
+	} else {
+		logger.Log(">>> skipping image copy")
+	}
+
 	restConfig, err := newRestConfig()
 	if err != nil {
 		return nil, nil, err
@@ -92,10 +111,16 @@ func setupCluster(ctx context.Context, t testing.TB) (kubernetes.Interface, vers
 		return nil, nil, err
 	}
 
-	if err := createResources(ctx, c); err != nil {
+	logger.Log(">>> deploying static resources")
+	if err := createResources(ctx, c, logger); err != nil {
 		return nil, nil, err
 	}
 	return kubeClient, opClient, nil
+}
+
+type log interface {
+	Log(args ...any)
+	Logf(format string, args ...any)
 }
 
 func newRestConfig() (*rest.Config, error) {
@@ -127,7 +152,7 @@ func newScheme() (*runtime.Scheme, error) {
 	return scheme, nil
 }
 
-func createResources(ctx context.Context, kubeClient client.Client) error {
+func createResources(ctx context.Context, kubeClient client.Client, logger log) error {
 	if err := deploy.CreateResources(context.Background(), kubeClient, deploy.WithMeta(projectID, cluster, location), deploy.WithDisableGCM(skipGCM)); err != nil {
 		return err
 	}
@@ -145,12 +170,90 @@ func createResources(ctx context.Context, kubeClient client.Client) error {
 		}
 	}
 
+	logger.Log(">>> waiting for operator to be deployed")
 	if err := kube.WaitForDeploymentReady(ctx, kubeClient, operator.DefaultOperatorNamespace, operator.NameOperator); err != nil {
 		return err
 	}
+	logger.Log(">>> waiting for operator to be ready")
 	if err := deploy.WaitForOperatorReady(ctx, kubeClient, operator.DefaultOperatorNamespace); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func imageExists(ctx context.Context, c *dockerclient.Client, image string) (bool, error) {
+	images, err := c.ImageList(ctx, types.ImageListOptions{
+		Filters: filters.NewArgs(filters.Arg("reference", image)),
+	})
+	if err != nil {
+		return false, err
+	}
+	if len(images) > 1 {
+		return false, errors.New("more than one image found")
+	}
+	return len(images) == 1, nil
+}
+
+func copyImagesToLocalRegistry(ctx context.Context, logger log, tag string, port int) error {
+	if tag == "" {
+		return errors.New("tag is required")
+	}
+	if port < 0 {
+		return fmt.Errorf("invalid port: %d", port)
+	}
+
+	dockerClient, err := dockerclient.NewClientWithOpts(dockerclient.FromEnv)
+	if err != nil {
+		return err
+	}
+	dockerClient.NegotiateAPIVersion(ctx)
+
+	for _, image := range images {
+		srcRoot := fmt.Sprintf("gmp/%s", image)
+		srcUntagged := fmt.Sprintf("%s:latest", srcRoot)
+		srcTagged := fmt.Sprintf("%s:%s", srcRoot, tag)
+
+		var src string
+		exists, err := imageExists(ctx, dockerClient, srcTagged)
+		if err != nil {
+			return err
+		}
+		if !exists {
+			logger.Logf(">>> using latest image: %s", image)
+			exists, err = imageExists(ctx, dockerClient, srcUntagged)
+			if err != nil {
+				return err
+			}
+			src = srcUntagged
+		} else {
+			src = srcTagged
+		}
+		if !exists {
+			logger.Logf(">>> skipping non-existent image: %s", image)
+			return nil
+		}
+
+		dst := fmt.Sprintf("localhost:%d/%s:%s", port, image, tag)
+		exists, err = imageExists(ctx, dockerClient, dst)
+		if err != nil {
+			return err
+		}
+		if exists {
+			logger.Logf(">>> skipping existent destination: %s", dst)
+			return nil
+		}
+
+		logger.Logf(">>> tagging and pushing image: %s", image)
+		if err := dockerClient.ImageTag(ctx, src, dst); err != nil {
+			return err
+		}
+		_, err = dockerClient.ImagePush(ctx, dst, types.ImagePushOptions{
+			RegistryAuth: "*",
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return err
 }
