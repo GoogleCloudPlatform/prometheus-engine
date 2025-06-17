8f34fa46ea9a0ffde4725de67781d6fdd352c4d9
bwplotka <bwplotka@google.com>
2025-06-17 14:50:06 +0100

    fix: update test after GCM improved PromQL compatibility
    Signed-off-by: bwplotka <bwplotka@google.com>

--- pkg/export/gcm/export_gcm_test.go
+++ pkg/export/gcm/export_gcm_test.go
@@ -63,18 +63,17 @@ func TestExport_CounterReset(t *testing.T) {
 			Expect(250, c, prom)
 
 		// Reset to 0 (simulating instrumentation resetting metric or restarting target).
+		// NOTE: This is not affected by b/259261536 as we notice the drop on the collector/export side
+		// and deduce correct CT from it.
 		counter.Reset()
 		c = counter.WithLabelValues("bar")
 		scrape(interval).
-			// NOTE(bwplotka): This and following discrepancies are expected due to
-			// GCM PromQL layer using MQL with delta alignment. What we get as a raw
-			// counter is already reset-normalized (b/305901765) (plus cannibalization).
-			Expect(50, c, export).
+			Expect(0, c, export).
 			Expect(0, c, prom)
 
 		c.Add(150)
 		scrape(interval).
-			Expect(200, c, export).
+			Expect(150, c, export).
 			Expect(150, c, prom)
 
 		// Reset to 0 with addition.
@@ -82,17 +81,17 @@ func TestExport_CounterReset(t *testing.T) {
 		c = counter.WithLabelValues("bar")
 		c.Add(20)
 		scrape(interval).
-			Expect(220, c, export).
+			Expect(20, c, export).
 			Expect(20, c, prom)
 
 		c.Add(50)
 		scrape(interval).
-			Expect(270, c, export).
+			Expect(70, c, export).
 			Expect(70, c, prom)
 
 		c.Add(10)
 		scrape(interval).
-			Expect(280, c, export).
+			Expect(80, c, export).
 			Expect(80, c, prom)
 
 		// Tricky reset case, unnoticeable reset for Prometheus without created timestamp as well.
@@ -100,7 +99,7 @@ func TestExport_CounterReset(t *testing.T) {
 		c = counter.WithLabelValues("bar")
 		c.Add(600)
 		scrape(interval).
-			Expect(800, c, export).
+			Expect(600, c, export).
 			Expect(600, c, prom)
 	})
 
