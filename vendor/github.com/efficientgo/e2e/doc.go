ee6f076ef64ee82616d22a4dfaa0bd49243eabc2
bwplotka <bwplotka@google.com>
2025-06-24 08:59:31 +0100

    chore: remove Go vendoring; allow automated -mod choice
    This change removed Go vendoring as we moved that part to Louhi import time. As a result
    we can simplify our repo worklows.
    
    See comments on docker images how conditional vendoring use was implemented.
    Tested localy with operator image, rest will be tested by CI.
    
    I did make some images more consistent on the way (allow-list of copied files).
    
    Signed-off-by: bwplotka <bwplotka@google.com>

--- vendor/github.com/efficientgo/e2e/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) The EfficientGo Authors.
-// Licensed under the Apache License 2.0.
-
-package e2e
-
-// This module is a fully featured e2e suite allowing hermetic complex microservice scenario for testing, benchmarking
-// or demo purposes using docker.
-//
-// See examples/prometheus to see example usage.
