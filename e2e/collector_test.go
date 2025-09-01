b7b5b8f2047305d6320cdcbfb4e63824a0904562
bwplotka <bwplotka@google.com>
2025-09-01 12:19:34 +0100

    feat: enable against GCM tests for e2e
    We plan to add more tests around this path, so checking if it's safe to
    enable or we need to do bit more work to make it per CI.
    
    We already have secret for other GCM e2e tests.
    
    Signed-off-by: bwplotka <bwplotka@google.com>

--- e2e/collector_test.go
+++ e2e/collector_test.go
@@ -22,7 +22,6 @@ import (
 	"testing"
 	"time"
 
-	gcm "cloud.google.com/go/monitoring/apiv3/v2"
 	gcmpb "cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 	"github.com/GoogleCloudPlatform/prometheus-engine/e2e/kube"
 	monitoringv1 "github.com/GoogleCloudPlatform/prometheus-engine/pkg/operator/apis/monitoring/v1"
@@ -458,7 +457,7 @@ func testValidateCollectorUpMetrics(ctx context.Context, kubeClient client.Clien
 		t.Log("checking for metrics in Cloud Monitoring")
 
 		// Wait for metric data to show up in Cloud Monitoring.
-		metricClient, err := gcm.NewMetricClient(ctx)
+		metricClient, err := newMetricClient(ctx)
 		if err != nil {
 			t.Fatalf("create metric client: %s", err)
 		}
@@ -537,7 +536,7 @@ func testCollectorScrapeKubelet(ctx context.Context, kubeClient client.Client) f
 		t.Log("checking for metrics in Cloud Monitoring")
 
 		// Wait for metric data to show up in Cloud Monitoring.
-		metricClient, err := gcm.NewMetricClient(ctx)
+		metricClient, err := newMetricClient(ctx)
 		if err != nil {
 			t.Fatalf("create GCM metric client: %s", err)
 		}
