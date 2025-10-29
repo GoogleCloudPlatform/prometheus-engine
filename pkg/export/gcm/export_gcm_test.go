8c71c7782659572848d4ed0c59babeed326f21fe
bwplotka <bwplotka@google.com>
2025-10-29 12:50:16 +0000

    fix: update GCM tests after GCM fixed delta alignment issue
    Signed-off-by: bwplotka <bwplotka@google.com>

--- pkg/export/gcm/export_gcm_test.go
+++ pkg/export/gcm/export_gcm_test.go
@@ -66,15 +66,12 @@ func TestExport_CounterReset(t *testing.T) {
 		counter.Reset()
 		c = counter.WithLabelValues("bar")
 		scrape(interval).
-			// NOTE(bwplotka): This and following discrepancies are expected due to
-			// GCM PromQL layer using MQL with delta alignment. What we get as a raw
-			// counter is already reset-normalized (b/305901765) (plus cannibalization).
-			Expect(50, c, export).
+			Expect(0, c, export).
 			Expect(0, c, prom)
 
 		c.Add(150)
 		scrape(interval).
-			Expect(200, c, export).
+			Expect(150, c, export).
 			Expect(150, c, prom)
 
 		// Reset to 0 with addition.
@@ -82,17 +79,17 @@ func TestExport_CounterReset(t *testing.T) {
 		c = counter.WithLabelValues("bar")
 		c.Add(20)
 		scrape(interval).
-			Expect(220, c, export).
+			Expect(20, c, export).
 			Expect(20, c, prom)
 
 		c.Add(50)
 		scrape(interval).
-			Expect(270, c, export).
+			Expect(70, c, export).
 			Expect(70, c, prom)
 
 		c.Add(10)
 		scrape(interval).
-			Expect(280, c, export).
+			Expect(80, c, export).
 			Expect(80, c, prom)
 
 		// Tricky reset case, unnoticeable reset for Prometheus without created timestamp as well.
@@ -100,7 +97,7 @@ func TestExport_CounterReset(t *testing.T) {
 		c = counter.WithLabelValues("bar")
 		c.Add(600)
 		scrape(interval).
-			Expect(800, c, export).
+			Expect(600, c, export).
 			Expect(600, c, prom)
 	})
 
