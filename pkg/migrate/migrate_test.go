70d2b552cc5df9f526c6f03dd79e9622908d2842
Karthik Unnikrishnan <kunnikrishnan@google.com>
2026-06-17 19:37:07 +0000

    build: add copyright license headers to go files

--- pkg/migrate/migrate_test.go
+++ pkg/migrate/migrate_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package migrate
 
 import (
@@ -53,11 +67,7 @@ func (d *DummyConverter) Convert(unstruct *unstructured.Unstructured, cache *Res
 }
 
 func TestMigratorCacheAndExtensibility(t *testing.T) {
-	tmpDir, err := os.MkdirTemp("", "migrate-test-*")
-	if err != nil {
-		t.Fatalf("failed to create temp dir: %v", err)
-	}
-	defer os.RemoveAll(tmpDir)
+	tmpDir := t.TempDir()
 
 	yamlContent := `
 apiVersion: monitoring.coreos.com/v1
