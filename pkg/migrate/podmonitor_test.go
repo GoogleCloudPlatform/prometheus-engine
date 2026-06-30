e559a17d7740df1d87d1ad5647f5f5962e38809d
Karthik Unnikrishnan <kunnikrishnan@google.com>
2026-06-30 18:45:13 +0000

    feat: metricRelabeling migration

--- pkg/migrate/podmonitor_test.go
+++ pkg/migrate/podmonitor_test.go
@@ -26,6 +26,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func TestPodMonitorConversion(t *testing.T) {
@@ -232,6 +233,228 @@ func TestPodMonitorConversion(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "Valid Basic Mapping & Capping & Defaulting",
+			input: &pomonitoringv1.PodMonitor{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "monitoring.coreos.com/v1",
+					Kind:       KindPodMonitor,
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "frontend-monitor",
+					Namespace: "frontend",
+				},
+				Spec: pomonitoringv1.PodMonitorSpec{
+					Selector: metav1.LabelSelector{
+						MatchLabels: map[string]string{"app": "frontend"},
+					},
+					PodMetricsEndpoints: []pomonitoringv1.PodMetricsEndpoint{
+						{
+							Port:          "web",
+							Path:          "/telemetry",
+							Scheme:        "HTTPS",
+							Interval:      "15s",
+							ScrapeTimeout: "10s",
+							Params:        map[string][]string{"debug": {"true"}},
+						},
+						{
+							TargetPort:    &intstr.IntOrString{Type: intstr.Int, IntVal: 8080},
+							Interval:      "10s",
+							ScrapeTimeout: "15s",
+						},
+						{
+							Port: "metrics",
+						},
+					},
+				},
+			},
+			expected: []runtime.Object{
+				&monitoringv1.PodMonitoring{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "monitoring.googleapis.com/v1",
+						Kind:       KindPodMonitoring,
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "frontend-monitor",
+						Namespace: "frontend",
+					},
+					Spec: monitoringv1.PodMonitoringSpec{
+						Selector: metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"app": "frontend",
+							},
+						},
+						Endpoints: []monitoringv1.ScrapeEndpoint{
+							{
+								Port:     intstr.FromString("web"),
+								Path:     "/telemetry",
+								Scheme:   "https",
+								Interval: "15s",
+								Timeout:  "10s",
+								Params:   map[string][]string{"debug": {"true"}},
+							},
+							{
+								Port:     intstr.FromInt(8080),
+								Interval: "10s",
+								Timeout:  "10s",
+							},
+							{
+								Port:     intstr.FromString("metrics"),
+								Interval: "30s",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Target Labels Mapping",
+			input: &pomonitoringv1.PodMonitor{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "monitoring.coreos.com/v1",
+					Kind:       KindPodMonitor,
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "label-monitor",
+					Namespace: "frontend",
+				},
+				Spec: pomonitoringv1.PodMonitorSpec{
+					JobLabel:        "app-name",
+					PodTargetLabels: []string{"env", "instance", "version"},
+					PodMetricsEndpoints: []pomonitoringv1.PodMetricsEndpoint{
+						{
+							Port: "metrics",
+						},
+					},
+				},
+			},
+			expected: []runtime.Object{
+				&monitoringv1.PodMonitoring{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "monitoring.googleapis.com/v1",
+						Kind:       KindPodMonitoring,
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "label-monitor",
+						Namespace: "frontend",
+					},
+					Spec: monitoringv1.PodMonitoringSpec{
+						Endpoints: []monitoringv1.ScrapeEndpoint{
+							{
+								Port:     intstr.FromString("metrics"),
+								Interval: "30s",
+							},
+						},
+						TargetLabels: monitoringv1.TargetLabels{
+							FromPod: []monitoringv1.LabelMapping{
+								{From: "env"},
+								{From: "instance", To: "exported_instance"},
+								{From: "version"},
+								{From: "app-name", To: "exported_job"},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Relabeling and Unsupported Warnings Mapping",
+			input: &pomonitoringv1.PodMonitor{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "monitoring.coreos.com/v1",
+					Kind:       KindPodMonitor,
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "relabel-monitor",
+					Namespace: "frontend",
+				},
+				Spec: pomonitoringv1.PodMonitorSpec{
+					Selector: metav1.LabelSelector{
+						MatchLabels: map[string]string{"app": "relabel-app"},
+					},
+					PodMetricsEndpoints: []pomonitoringv1.PodMetricsEndpoint{
+						{
+							Port:            "metrics",
+							HonorLabels:     true,
+							HonorTimestamps: ptrTo(true),
+							MetricRelabelConfigs: []pomonitoringv1.RelabelConfig{
+								{
+									SourceLabels: []pomonitoringv1.LabelName{"__name__"},
+									TargetLabel:  "instance",
+									Action:       "Replace",
+								},
+								{
+									SourceLabels: []pomonitoringv1.LabelName{"container"},
+									TargetLabel:  "container_name",
+									Action:       "replace",
+								},
+								{
+									SourceLabels: []pomonitoringv1.LabelName{"temp"},
+									Action:       "LabelMap",
+								},
+								{
+									SourceLabels: []pomonitoringv1.LabelName{"namespace"},
+									Regex:        "default",
+									Action:       "keep",
+								},
+								{
+									SourceLabels: []pomonitoringv1.LabelName{"job"},
+									Regex:        "api-.*",
+									Action:       "drop",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: []runtime.Object{
+				&monitoringv1.PodMonitoring{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "monitoring.googleapis.com/v1",
+						Kind:       KindPodMonitoring,
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "relabel-monitor",
+						Namespace: "frontend",
+					},
+					Spec: monitoringv1.PodMonitoringSpec{
+						Selector: metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"app": "relabel-app",
+							},
+						},
+						Endpoints: []monitoringv1.ScrapeEndpoint{
+							{
+								Port:     intstr.FromString("metrics"),
+								Interval: "30s",
+								MetricRelabeling: []monitoringv1.RelabelingRule{
+									{
+										SourceLabels: []string{"__name__"},
+										TargetLabel:  "exported_instance",
+										Action:       "replace",
+									},
+									{
+										SourceLabels: []string{"container"},
+										TargetLabel:  "container_name",
+										Action:       "replace",
+									},
+									{
+										SourceLabels: []string{"namespace"},
+										Regex:        "default",
+										Action:       "keep",
+									},
+									{
+										SourceLabels: []string{"job"},
+										Regex:        "api-.*",
+										Action:       "drop",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
 	converter := &PodMonitorConverter{}
@@ -294,6 +517,10 @@ func toUnstructured(t *testing.T, obj any) *unstructured.Unstructured {
 		t.Fatalf("failed to convert object to unstructured: %v", err)
 	}
 	return &unstructured.Unstructured{Object: m}
+}
+
+func ptrTo[T any](v T) *T {
+	return &v
 }
 
 type testingWriter struct {
